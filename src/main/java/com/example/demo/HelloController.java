06a37e7a9009e44917eb63e5d637027341e21dc6
Soudeh Gholami <soudehschl@Soudehs-Air.fritz.box>
2022-06-22 11:16:28 +0200

    edited controller

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
     @RestController
     public class HelloController {
-
+@Autowired
         @GetMapping("/hello")
         public String sayHello() {
             return "Guten Tag. Wie geht es dir? Ma name ist Soudeh. ";
